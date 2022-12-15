3ea0e00f32f506e95732500af5eb37fbff8e56fe
Erwan <erwan.ngma@gmail.com>
2022-12-15 09:52:10 +0100

    Mario movement update

--- js/main.js
+++ js/main.js
@@ -80,38 +80,13 @@ function create() {
     player.setCollideWorldBounds(true); // ne peux pas d�passer la carte
 
     this.gumba3 = new Gumba(this, groundLayer, 50, 550);
-    // this.gumba3.createObject(this, groundLayer, 50, 550)
-    this.gumba3.collideWithPlayer(this, player, death, lifescore, lifecount)
+    this.gumba3.collideWithPlayer(this, player, death, lifescore, lifecount);
 
     this.gumba1 = new Gumba(this, groundLayer, 100, 550);
-    // this.gumba1.createObject(this, groundLayer, 100, 550)
-    this.gumba1.collideWithPlayer(this, player, death, lifescore, lifecount)
+    this.gumba1.collideWithPlayer(this, player, death, lifescore, lifecount);
 
     this.gumba2 = new Gumba(this, groundLayer, 300, 550);
-    // this.gumba2.createObject(this, groundLayer, 300, 550)
-    this.gumba2.collideWithPlayer(this, player, death, lifescore, lifecount)
-
-
-
-
-
-
-
-
-    /*//ajouter gumba
-    gumba = this.physics.add.sprite(100,550,'gumba');
-    //gumba.setBounce(0.2);
-    gumba.setCollideWorldBounds(true);
-    this.physics.add.collider(groundLayer, gumba);*/
-
-    /*//Mort d'un gumba
-    gumbadead = this.physics.add.sprite(150,550,'gumbadead');
-    this.physics.add.collider(groundLayer,gumbadead);*/
-
-    //mario life
-    /*life = this.add.sprite(40,80,'life',{height : '20 px'});
-    life1 = this.add.sprite(110,80,'life',{height : '20 px'});
-    life2 = this.add.sprite(180,80,'life',{height : '20 px'});*/
+    this.gumba2.collideWithPlayer(this, player, death, lifescore, lifecount);
 
     // small fix to our player images, we resize the physics body object slightly
     player.body.setSize(player.width, player.height - 8);
@@ -178,11 +153,6 @@ function collectCoin(sprite, tile) {
     text.setText(score); // set the text to show the current score
     return false;
 }
-
-/*function removeGumba(gumba){
-    gumba.anims.play('gumbadeath',true);
-    //console.log("test");
-}*/
 
 function update(time, delta) {
 
