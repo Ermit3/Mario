66515498090e263e1f2a6c8fc450eb0f15757e53
Erwan <erwan.ngma@gmail.com>
2022-12-22 13:30:49 +0100

    Mario Game Over

--- js/main.js
+++ js/main.js
@@ -44,8 +44,9 @@ function preload() {
     // coin
     this.load.image('coin', 'assets/coinGold.png');
     // player animations
-    this.load.atlas('player', 'assets/player2.png', 'assets/player.json');
+    this.load.atlas('player', 'assets/opti_player.png', 'assets/opti_player.json');
     this.load.atlas('gumba', 'assets/spritegumba.png', 'assets/spritegumba.json');
+    this.load.atlas('death', 'assets/player3.png', 'assets/player3.json');
 
     //this.load.image('gumba','assets/gumba.png');
     this.load.image('life', 'assets/life.png');
@@ -65,33 +66,29 @@ function create() {
     // collision avec le groundLayer
     groundLayer.setCollisionByExclusion([-1]);
 
-
+    // GameOver
+    this.physics.resume(); // Après gameover si le monde est en pause, il reprendra
+    this.gameOver = false;
 
     // add coins
     var coinTiles = map.addTilesetImage('coin');
     coinLayer = map.createDynamicLayer('Coins', coinTiles, 0, 0);
 
+    // GESTION BORDURE
     // set the boundaries of our game world
     this.physics.world.bounds.width = groundLayer.width;
     this.physics.world.bounds.height = groundLayer.height;
+    // FIN BORDURE //
 
-    // ajout player     
-    // player2 = this.physics.add.sprite(200, 200, 'player');
-    // //player.setBounce(0.2); // our player will bounce from items
-    // player.setCollideWorldBounds(true); // ne peux pas d�passer la carte
-    // player.isAlive = true;
-    // player.lifescore = 3;
-    // player.isGhost = false;
-
+    // GESTION APPARITION
+    /// MARIO
     mario = new Mario(this, groundLayer, 200, 200);
     player = mario.player;
-
+    /// GUMBA
     this.gumba3 = new Gumba(this, groundLayer, 50, 550);
-    // this.gumba3.collideWithPlayer(this, player);
     this.gumba1 = new Gumba(this, groundLayer, 100, 550);
-    // this.gumba1.collideWithPlayer(this, player);
     this.gumba2 = new Gumba(this, groundLayer, 300, 550);
-    // this.gumba2.collideWithPlayer(this, player);
+    // FIN APPARITION //
 
     // small fix to our player images, we resize the physics body object slightly
     player.body.setSize(player.width, player.height - 8);
@@ -100,18 +97,23 @@ function create() {
     mario.collideWithEnemy(this, this.gumba2.gumba);
     mario.collideWithEnemy(this, this.gumba3.gumba);
 
+    // PLAYER PHYSICS
     // player will collide with the level tiles 
     this.physics.add.collider(groundLayer, player);
+    // FIN PLAYER PHYSICS //
 
 
     coinLayer.setTileIndexCallback(7, collectCoin, this);
     // // when the player overlaps with a tile with index 17, collectCoin 
     // // will be called    
     this.physics.add.overlap(player, coinLayer);
+
+    // GESTION DES SPRITES
+    /// MARIO
     // player walk animation
     this.anims.create({
         key: 'walk',
-        frames: this.anims.generateFrameNames('player', { prefix: 'p1_walk', start: 1, end: 11, zeroPad: 2 }),
+        frames: this.anims.generateFrameNames('player', { prefix: 'p1_walk', start: 3, end: 1, zeroPad: 2 }),
         frameRate: 10,
         repeat: -1
     });
@@ -121,23 +123,39 @@ function create() {
         frames: [{ key: 'player', frame: 'p1_stand' }],
         frameRate: 10,
     });
-
-    //gumba animation
+    // 
+    this.anims.create({
+        key: 'jump',
+        frames: [{ key: 'player', frame: 'p1_jump' }],
+        frameRate: 10,
+    });
+    // 
+    this.anims.create({
+        key: 'death',
+        frames: [{ key: 'player', frame: 'p1_death' }],
+        frameRate: 10,
+    });
+    ///GUMBA
     this.anims.create({
         key: 'gumbadeath',
         frames: [{ key: 'gumba', frame: 'sprite25' }],
     })
+    // FIN SPRITE //
 
     cursors = this.input.keyboard.createCursorKeys();
 
+    // GESTION DE CAMERA
     // set bounds so the camera won't go outside the game world
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
     // make the camera follow the player
     this.cameras.main.startFollow(player);
-
     // set background color, so the sky is not black    
     this.cameras.main.setBackgroundColor('#ccccff');
+    // à coriger
+    this.cameras.main.fadeIn(1000);
+    // FIN CAMERA //
 
+    // GESTION DU TEXT
     // this text will show the score
     text = this.add.text(20, 570, '0', {
         fontSize: '20px',
@@ -152,6 +170,28 @@ function create() {
     });
     // fix the text to the camera
     lifecount.setScrollFactor(0);
+
+    ///GameOver
+    this.gameOverText = this.add.text(400, 200, "GAME OVER", {
+        fontSize: '100px',
+        fill: '#000',
+    });
+    this.gameOverText.setOrigin(0.5);
+    this.gameOverText.depth = 2;
+    this.gameOverText.visible = false;
+    this.gameOverText.setScrollFactor(0);
+    this.gameOverText.setStroke('#000', 5)
+    /// RESTART
+    this.restartText = this.add.text(400, 300, "Press 'space' to restart", {
+        fontSize: '30px',
+        fill: '#000'
+    });
+    this.restartText.setOrigin(0.5);
+    this.restartText.depth = 2;
+    this.restartText.visible = false;
+    this.restartText.setScrollFactor(0);
+    this.restartText.setStroke('#000', 2)
+    // FIN TEXT //
 }
 
 
@@ -165,13 +205,21 @@ function collectCoin(sprite, tile) {
 
 function update(time, delta) {
 
+    if (this.gameOver && cursors.space.isDown) {
+        mario.restartScene(this);
+    } else if (this.gameOver) {
+        return
+    }
+
     this.gumba1.changeDirection();
     this.gumba2.changeDirection();
     this.gumba3.changeDirection();
 
     // MarioMove(player, cursors);
-    mario.playerMove(player, cursors);
+    mario.playerMove(player, cursors, this);
 
     // MarioDeath(player);
-    mario.playerDeath(player);
+    mario.playerDeath(player, this);
+
+    // mario.playerDead(player);
 }
--- js/mario.js
+++ js/mario.js
@@ -2,8 +2,8 @@ function isGhost(player) {
     player.alpha = player.alpha == 1 ? 0.2 : 1;
     player.isGhost = player.isGhost == false ? true : false;
 
-    // for (var i = 0; i < 10; i++) {
-    //     i % 2 == 0 ? setTimeout(() => { this.Ghost(1) }, this.blueGhostTimer + i * 100) : setTimeout(() => { this.Ghost(0) }, this.blueGhostTimer + i * 100);
+    // for (var i = 0; i <= 10; i++) {
+    //     i % 2 == 0 ? setTimeout(() => { player.alpha = 1; i == 10 ? player.isGhost = false : console.log("k"); }, i * 100) : setTimeout(() => { player.alpha = 0.1 }, i * 100);
     // }
 }
 
@@ -16,11 +16,12 @@ export default class Mario {
         this.player.setCollideWorldBounds(true);
         main.physics.add.collider(groundLayer, this.player);
         this.player.isAlive = true;
-        this.player.lifescore = 3;
+        this.player.lifescore = 1;
         this.player.isGhost = false;
+        this.player.depth = 1; // z-index du texte
     }
 
-    playerMove(player, cursors) {
+    playerMove(player, cursors, main) {
         if (player.isAlive) {
             if (cursors.left.isDown) {
                 player.body.setVelocityX(-200);
@@ -35,18 +36,26 @@ export default class Mario {
                 player.body.setVelocityX(0);
                 player.anims.play('idle', true);
             }
-            // jump 
+            // jump
+            if (!player.body.onFloor()) {
+                player.anims.play('jump', true); // Active when player not on the ground
+            }
             if (cursors.up.isDown && player.body.onFloor()) {
                 player.body.setVelocityY(-400);
-                console.log(player.lifescore);
-                console.log(player.isAlive);
+            }
+            // Debug cursor
+            if (cursors.down.isDown && player.body.onFloor()) {
+                console.log("Lifecount " + player.lifescore);
+                console.log("Alive " + player.isAlive);
+                console.log("Ghost " + player.isGhost);
+                console.log(main);
             }
         }
     }
 
     collideWithEnemy(main, enemy) {
         main.physics.add.overlap(this.player, enemy, function (player, enemy) {
-            if (player.y + 15.5 < enemy.y) {
+            if (player.y + 15.5 < enemy.y && player.isAlive == true) {
                 player.body.setVelocityY(-200);
                 enemy.anims.play('gumbadeath', true);
                 enemy.setVelocity(0, 0);
@@ -63,77 +72,35 @@ export default class Mario {
         });
     }
 
-    playerDeath(player) {
+    playerDeath(player, main) {
         if (player.enemyTouch === true) {
             if (player.lifescore > 0) {
                 player.lifescore = player.lifescore - 1;
                 console.log(player.lifescore);
-                player.enemyTouch = false
-                isGhost(player)
+                player.enemyTouch = false // 
+                isGhost(player) // Frame d'invinsibilité
                 setTimeout(() => {
                     isGhost(player)
                 }, 2000);
             } else {
                 player.isAlive = false;
+                player.body.setVelocityX(0);
+                // player.body.setVelocityY(-50);
+                player.anims.play('death', true);
                 console.log("DEAD");
-                setTimeout(() => {
-                    player.destroy();
-                }, 1000);
+                // main.cameras.main.fade(1000, 255, 255, 255);
+                main.gameOver = true;
+                main.gameOverText.visible = true;
+                main.restartText.visible = true;
+                main.physics.pause();
+                // setTimeout(() => {
+                //     main.scene.start('main');
+                // }, 1500);
             }
         }
     }
+
+    restartScene(main) {
+        main.scene.start('main');
+    }
 }
-
-
-// function MarioMove(player, cursors) {
-//     if (player.isAlive) {
-//         if (cursors.left.isDown) {
-//             player.body.setVelocityX(-200);
-//             player.anims.play('walk', true); // walk left
-//             player.flipX = true; // flip the sprite to the left
-//         }
-//         else if (cursors.right.isDown) {
-//             player.body.setVelocityX(200);
-//             player.anims.play('walk', true);
-//             player.flipX = false; // use the original sprite looking to the right
-//         } else {
-//             player.body.setVelocityX(0);
-//             player.anims.play('idle', true);
-//         }
-//         // jump
-//         if (cursors.up.isDown && player.body.onFloor()) {
-//             player.body.setVelocityY(-400);
-//             console.log(player.lifescore);
-//             console.log(player.isAlive);
-//         }
-//     }
-// }
-
-// function isGhost(player) {
-//     player.alpha = player.alpha == 1 ? 0.2 : 1;
-//     player.isGhost = player.isGhost == false ? true : false;
-
-//     // for (var i = 0; i < 10; i++) {
-//     //     i % 2 == 0 ? setTimeout(() => { this.Ghost(1) }, this.blueGhostTimer + i * 100) : setTimeout(() => { this.Ghost(0) }, this.blueGhostTimer + i * 100);
-//     // }
-// }
-
-// function MarioDeath(player) {
-//     if (player.enemyTouch === true) {
-//         if (player.lifescore > 0) {
-//             player.lifescore = player.lifescore - 1;
-//             console.log(player.lifescore);
-//             player.enemyTouch = false
-//             isGhost(player)
-//             setTimeout(() => {
-//                 isGhost(player)
-//             }, 2000);
-//         } else {
-//             player.isAlive = false;
-//             console.log("DEAD");
-//             player.destroy();
-//         }
-//     }
-// }
-
-// export { MarioMove, MarioDeath }
