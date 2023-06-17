b696e5582692d43d4b25c61e5929e32008610086
tommylee1999 <73222982+tommylee1999@users.noreply.github.com>
2023-06-17 17:36:07 +0200

    Update enemy.js

--- js/enemy.js
+++ js/enemy.js
@@ -11,25 +11,6 @@ export default class Enemy{
         main.physics.add.collider(groundLayer, this.enemy);
     }
 
-
-    /* ShellMovement(){
-        if (this.enemy.enemyAlive == false && this.KoopaMario == false){
-        this. enemy.setVelocity(100, 0);
-            if (this. enemy.body.blocked.right) {
-                this. enemy.direction = 'left';
-            }
-            if (this. enemy.body.blocked.left) {
-                this. enemy.direction = 'right';
-            }
-            if (this. enemy.direction === 'right') {
-                this. enemy.setVelocity(100, 0);
-            }
-            if (this. enemy.direction === 'left') {
-                this. enemy.setVelocity(-100, 0);
-            }
-        }
-    } */
-
     changeDirection() {
         
         if (this.enemy.enemyAlive == true) {
@@ -49,6 +30,23 @@ export default class Enemy{
             }
         }
     }
+/* ShellMovement(){
+        if (this.enemy.enemyAlive == false && this.KoopaMario == false){
+        this. enemy.setVelocity(100, 0);
+            if (this. enemy.body.blocked.right) {
+                this. enemy.direction = 'left';
+            }
+            if (this. enemy.body.blocked.left) {
+                this. enemy.direction = 'right';
+            }
+            if (this. enemy.direction === 'right') {
+                this. enemy.setVelocity(100, 0);
+            }
+            if (this. enemy.direction === 'left') {
+                this. enemy.setVelocity(-100, 0);
+            }
+        }
+    } */
 
     /* CollideShellGumba(){
         main.physics.add.overlap(this.koopa, this.gumba, function (koopa, gumba){
